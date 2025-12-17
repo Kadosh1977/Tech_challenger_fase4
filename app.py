fddfeb97196157ff5236b160a7e55035d1891b22
Kadosh1977 <alexgomes1976@gmail.com>
2025-12-16 23:32:30 -0300

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Previsão IBOVESPA (CatBoost)", layout="centered"
 st.title("📈 Tendência IBOVESPA - CatBoost")
 
 CSV_FILE = "base_de_dados.csv"
-THRESHOLD = 0.55
+THRESHOLD = 0.50
 TEST_SIZE = 30
 
 # ==============================
