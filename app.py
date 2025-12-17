ced50546dd7678c4633fe5cf1bd103b9f938a7bf
Kadosh1977 <alexgomes1976@gmail.com>
2025-12-16 21:47:36 -0300

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import csv
 st.set_page_config(page_title="Previsão IBOVESPA (CatBoost)", layout="centered")
 st.title("📈 Tendência IBOVESPA - CatBoost")
 
-CSV_FILE = "Dados Históricos - Ibovespa 20 anos.csv"
+CSV_FILE = "base_de_dados"
 THRESHOLD = 0.55
 TEST_SIZE = 30
 LOG_FILE = "log_previsoes.csv"
