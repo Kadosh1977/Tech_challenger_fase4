1bb13c9042c5129ca09973a0e309f62804e8a980
Alexandre Gomes <alexgomes1976@gmail.com>
2025-12-27 20:19:45 -0300

    app.py

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ st.sidebar.markdown(
 
 st.sidebar.markdown(
     """
-    <div style="text-align: justify; font-size: 12px">
-        O aplicativo é inicialmente carregado com dados de backtest.
+    <div style="text-align: justify; font-size: 13px">
+        O aplicativo é inicialmente carregado com dados de backtest.<br></br>
         Para realizar uma nova análise (ex: 12 meses), envie um
         arquivo CSV exportado do <strong>Investing.com</strong>, contendo
         no mínimo 18 meses de histórico.
